d9ee84faab692c5842c2dc0c0d2566ce5ecc9937
Justin Mattos <justin.mattos@nyu.edu>
2021-05-27 22:10:38 -0400

    added nested user routers and created dynamically generated auth routes

--- server/api/user.js
+++ server/api/user.js
@@ -3,6 +3,12 @@ const router = Router();
 
 // This router is mounted at /api/user
 
-// Routes
+// High-level User Routes
+
+// Nested routers for the user api
+const { userAuth } = require('./auth');
+router.use('/auth', userAuth);
+router.use('/order', require('./user/order'));
+router.use('/tab', require('./user/tab'));
 
 module.exports = router;
