5bbe137cc56a371df3e1cdcd06af3e80288918b0
Arjan Mitra <arjanmitra@hotmail.com>
2021-05-31 16:11:32 -0400

    first commit

--- server/api/venue.js
+++ server/api/venue.js
@@ -1,8 +1,18 @@
 const { Router } = require('express');
+const Venue = require('../db/models/Venue');
 const router = Router();
 
 // This router is mounted at /api/venue
 
 // Routes
+
+router.get('/', async (req, res, next) => {
+  try {
+    const venues = await Venue.findAll();
+    res.send(venues);
+  } catch (error) {
+    next(error);
+  }
+});
 
 module.exports = router;
