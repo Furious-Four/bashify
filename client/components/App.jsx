1c48f2e8d610e9ef055bbdc232187c38ca6e6530
Justin Mattos <justin.mattos@nyu.edu>
2021-06-08 21:00:34 -0400

    client-side file structure

---
+++ client/components/App.jsx
@@ -0,0 +1,7 @@
+// const App = () => {
+//   const [venue, setVenue] = useState();
+//   const fetchVenueDetails = async (id) => {
+//     const response = await axios.get(`/api/venue/${id}`);
+//     setVenue(response);
+//   };
+// };
