d09273f7ff2006fb2e07a9ddc63ea0972a01cbdb
Justin Mattos <justin.mattos@nyu.edu>
2021-06-10 21:00:47 -0400

    login and registration

--- client/components/App.jsx
+++ client/components/App.jsx
@@ -1,11 +1,49 @@
-import React from 'react';
-import { HashRouter as Router, Route, Switch } from 'react-router-dom';
+import React, { useState, useEffect } from 'react';
+import {
+  HashRouter as Router,
+  Route,
+  Switch,
+  Redirect,
+} from 'react-router-dom';
+import axios from 'axios';
+
+import Login from './authentication/Login.jsx';
+import Register from './authentication/Register.jsx';
 
 const App = () => {
+  const [user, setUser] = useState({});
+  const [loggedIn, setLoggedIn] = useState(false);
+
+  const fetchUserDetails = async (token) => {
+    try {
+      const { data: user } = await axios.get('/api/user', {
+        headers: { authorization: token },
+      });
+      setUser(user);
+    } catch (err) {
+      console.error(err);
+    }
+  };
+
+  useEffect(() => {
+    // Like componentDidMount and componentDidUpdate all in one
+    const token = window.localStorage.getItem('token');
+    if (token && !loggedIn) {
+      setLoggedIn(true);
+      fetchUserDetails(token);
+    }
+  });
+
   return (
     <Router>
       {/* MainNav */}
       <Switch>
+        <Route path="/login">
+          {loggedIn ? <Redirect to="/dashboard" /> : <Login />}
+        </Route>
+        <Route path="/register">
+          {loggedIn ? <Redirect to="/dashboard" /> : <Register />}
+        </Route>
         {/*
         AllDrinks
         SingleDrink
--- server/app.js
+++ server/app.js
@@ -13,4 +13,12 @@ app.use('/public', express.static(path.join(__dirname, '..', 'public')));
 app.use('/api/user', require('./api/user'));
 app.use('/api/venue', require('./api/venue'));
 
+app.get('/', async (req, res, next) => {
+  try {
+    res.sendFile(path.join(__dirname, '../public/index.html'));
+  } catch (err) {
+    next(err);
+  }
+});
+
 module.exports = app;
