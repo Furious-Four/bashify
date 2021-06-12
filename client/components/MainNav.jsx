34e2604624ba4e7a2c5413e6223c6de84cfc0f40
Justin Mattos <justin.mattos@nyu.edu>
2021-06-12 12:14:42 -0400

    main nav slideout

--- client/components/MainNav.jsx
+++ client/components/MainNav.jsx
@@ -1,23 +1,102 @@
 import React, { useState } from 'react';
-import { Link } from 'react-router-dom';
+import { Link, useHistory, useLocation } from 'react-router-dom';
 
-import { MainNavDiv } from '../styles/MainNav';
+import {
+  MainNavDiv,
+  NavSlideout,
+  NavTopbar,
+  SlideoutOptions,
+  SlideoutOpt,
+} from '../styles/MainNav';
 
 const MainNav = ({ user }) => {
-  console.log(user);
+  const [isExpanded, setExpanded] = useState(false);
+  const history = useHistory();
+  const location = useLocation().pathname.split('/')[1];
+
+  const expandMenu = (ev) => setExpanded(true);
+
+  const collapseMenu = (ev) => setExpanded(false);
+
+  const navigate = (path) => {
+    history.push(`/${path}`);
+    collapseMenu();
+  };
+
+  const logout = () => {
+    window.localStorage.removeItem('token');
+    history.push('/');
+    collapseMenu();
+  };
+
+  console.log(location);
   return (
     <MainNavDiv>
-      <div>
-        <img src="/public/menu.svg" width="20em" />
-      </div>
-      <Link to="/">bashify</Link>
-      <div>
-        {user.id ? (
-          <div>Welcome, {user.firstName}</div>
+      <NavTopbar>
+        {isExpanded ? (
+          <div>
+            <div></div>
+            <div onClick={collapseMenu}>
+              <img src="/public/exit.svg" width="20em" />
+            </div>
+          </div>
         ) : (
-          <Link to="/login">Login</Link>
+          <div>
+            <div onClick={expandMenu}>
+              <img src="/public/menu.svg" width="20em" />
+            </div>
+            <Link to="/">bashify</Link>
+            <div>
+              {user.id ? (
+                <div>Welcome, {user.firstName}</div>
+              ) : (
+                <Link to="/login">Login</Link>
+              )}
+            </div>
+          </div>
         )}
-      </div>
+      </NavTopbar>
+      {isExpanded ? (
+        <NavSlideout>
+          <SlideoutOptions>
+            <SlideoutOpt
+              current={location === 'profile'}
+              onClick={() => navigate('profile')}
+            >
+              Profile
+            </SlideoutOpt>
+            <SlideoutOpt
+              current={location === 'menu'}
+              onClick={() => navigate('menu')}
+            >
+              Menu
+            </SlideoutOpt>
+            <SlideoutOpt
+              current={location === 'order'}
+              onClick={() => navigate('order')}
+            >
+              Current Order
+            </SlideoutOpt>
+            <SlideoutOpt
+              current={location === 'tab'}
+              onClick={() => navigate('tab')}
+            >
+              Your Tab
+            </SlideoutOpt>
+            <SlideoutOpt
+              current={location === 'splits'}
+              onClick={() => navigate('splits')}
+            >
+              Your Splits
+            </SlideoutOpt>
+          </SlideoutOptions>
+          <SlideoutOpt current={location === 'logout'} onClick={logout}>
+            Log Out
+          </SlideoutOpt>
+        </NavSlideout>
+      ) : (
+        ''
+      )}
     </MainNavDiv>
   );
 };
