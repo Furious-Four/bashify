837498d066807446e4b7f56d1e5660e37965fc58
mmartin2755@gmail.com <mmartin2755@gmail.com>
2021-06-20 21:37:07 -0400

    data rendering

--- client/components/tabs/CurrentTab.jsx
+++ client/components/tabs/CurrentTab.jsx
@@ -2,39 +2,50 @@ import React, { useState, useEffect } from 'react';
 import axios from 'axios';
 
 const CurrentTab = () => {
-  const [tab, setTab] = useState([]);
-  //   const [loading, setLoading] = useState(true);
+  const [tab, setTab] = useState({});
+  const [drinks, setDrinks] = useState([]);
+  const [loading, setLoading] = useState(true);
 
   useEffect(async () => {
     try {
       const token = window.localStorage.getItem('token');
+
       const { data: tab } = await axios.get(`/api/user/tab/current`, {
         headers: { authorization: token },
       });
       setTab(tab);
-      //   setLoading(false);
-      console.log(tab);
+      setLoading(false);
+      const drinks = tab.tabDrinks;
+      setDrinks(drinks);
+      console.log('tab is', tab);
+      console.log('drinks is ', drinks);
     } catch (ex) {
       console.log(ex);
     }
   }, []);
-  //   if (loading) {
-  //     return <div>...loading </div>;
-  //   } else {
+
   return (
     <div>
       <h1> Current Tab </h1>
-      <h2> Drinks </h2>
 
-      <div> `tab is ${tab}` </div>
+      <h5>Drinks</h5>
       <hr />
-      <h4> SubTotal </h4>
-      <h5> Tax </h5>
-      <h6> Tip </h6>
-      <h4> Total </h4>
+      <h3> SubTotal </h3>
+      <div>
+        {drinks.map((drink) => {
+          return (
+            <div>
+              <div key={drink.drink.id}> {drink.drink.name} </div>
+              <div key={drink.drink.name}>{drink.drink.price} </div>
+              <div key={drink.drink.tabId}>{drink.drink.amount} </div>
+            </div>
+          );
+        })}
+      </div>
+      <h5> Tip {tab.tip} </h5>
+      <h2> Total </h2>
     </div>
   );
-  //   }
 };
 
 export default CurrentTab;
