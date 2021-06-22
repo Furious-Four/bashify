693aad4a77f44899b31d2620931aff6868b69ab9
Arjan Mitra <arjanmitra@hotmail.com>
2021-06-22 18:35:53 -0400

    completed inc and dec functionality

--- client/components/utils/IncDrink.js
+++ client/components/utils/IncDrink.js
@@ -5,17 +5,33 @@ export const incDrink = async (id) => {
     //check if user token exists, if not, redirect to login/register page. otherwise continue:
     //check if active tab already exists. if not, create new tab and new order and add on drink. if so, add onto existing order
     if (window.localStorage.token) {
+      const tokenObj = window.localStorage.token;
       const { data: activeOrder } = await axios.get('/api/user/order/current', {
-        headers: { authorization: window.localStorage.token },
+        headers: { authorization: tokenObj },
       });
-      if (activeOrder) {
-        //create a new orderDrink row
-        //const updatedOrder
+      ///if (activeOrder) {
+      //loop through orderDrinks and see if there is already a drink with id in there. if there is, increment that quantity by 1. otherwise, create a new orderDrink row.
+      const orderExists = activeOrder.orderDrinks.filter(
+        (orderDrink) => orderDrink.drinkId === id
+      );
+      if (orderExists.length > 0) {
+        const orderExistsQuantity = orderExists[0].quantity;
+        const updateDrinkOnOrder = await axios.put(
+          '/api/user/order/current/modify-drink',
+          { drinkId: id, quantity: orderExistsQuantity + 1 },
+          {
+            headers: { authorization: tokenObj },
+          }
+        );
       } else {
-        //create a new order row
-        //create a new orderDrink row
+        const newDrinkOnOrder = await axios.put(
+          '/api/user/order/current/modify-drink',
+          { drinkId: id, quantity: 1 },
+          {
+            headers: { authorization: tokenObj },
+          }
+        );
       }
-      console.log(activeOrder);
     } else {
       alert('you need to login!');
     }
