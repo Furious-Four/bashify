5e169c9e68e2a60d54b9078b8c61ab92e9b5dabf
Dominique Sobieski <sobieski.d@gmail.com>
2021-06-24 00:02:05 -0400

    charge on tab route working

--- client/components/utils/Checkout.js
+++ client/components/utils/Checkout.js
@@ -5,7 +5,7 @@ import {loadStripe} from '@stripe/stripe-js';
 import CardSetupForm from './CardSetupForm';
 
 const stripePromise = loadStripe('pk_test_51IvtaYFkob7fZ9T0uLozxOC3cAPrDRZzVn8FJSav6aVr9SWu5flNYUi5RlNIK3nimDNQSQr4DGs2EDGJPo8v8NrF00URtmrPHH', {
-  stripeAccount: '{{CONNECTED_STRIPE_ACCOUNT_ID}}'
+  //stripeAccount: '{{CONNECTED_STRIPE_ACCOUNT_ID}}'
 });
 
 const Checkout = () => {
